88e445022efa0aebe9d80781509d4af7a81097b9
AzizAbd <91793452+AzizAbd@users.noreply.github.com>
2022-10-31 17:03:40 +0100

    changement

--- cnx.php
+++ cnx.php
@@ -1,9 +1,6 @@
 <?php
 
-$user="root";
-$mdp="";
-$db="project1";
-$server ="localhost";
+
 
 $link = mysqli_connect($server,$user,$mdp,$db);
 
--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-＄host       = "us-cdbr-east-02.cleardb.com";
-＄username   = "your remote db username";
-＄password   = "your secrete";
-＄dbname     = "your db name";
+＄host       = "eu-cdbr-west-03.cleardb.net";
+＄username   = "b3ba1f2e8015b8";
+＄password   = "e2250b51";
+＄dbname     = "heroku_345c439606a8e71";
 
 if (＄_SERVER['HTTP_HOST'] == 'localhost') {
   ＄host       = "localhost";
